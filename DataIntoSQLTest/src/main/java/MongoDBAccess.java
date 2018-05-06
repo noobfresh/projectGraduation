588e1949f100528773ca6f88d439f5d1cc3af396
noobfresh <954078940@qq.com>
2018-05-06 23:58:55 +0800

    out put json

--- DataIntoSQLTest/src/main/java/MongoDBAccess.java
+++ DataIntoSQLTest/src/main/java/MongoDBAccess.java
@@ -3,7 +3,13 @@ import com.mongodb.client.FindIterable;
 import com.mongodb.client.MongoCollection;
 import com.mongodb.client.MongoCursor;
 import com.mongodb.client.MongoDatabase;
+import javafx.print.Printer;
 import org.bson.Document;
+
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.InputStream;
+import java.io.PrintWriter;
 
 public class MongoDBAccess {
 
@@ -20,9 +26,21 @@ public class MongoDBAccess {
 
             FindIterable<Document> doc = collection.find();
             System.out.println("==============================================================");
-            doc.limit(1);
+            doc.limit(7);
+            int countId = 0;
             for(Document temp : doc){
-                System.out.println(temp.toJson());
+                String json = temp.toJson();
+                String path = "C:\\Users\\PYF\\Desktop\\test\\busdata" + "\\" + countId + ".txt";
+                File file = new File(path);
+                if(!file.exists()){
+                    file.createNewFile();
+                }
+                FileOutputStream fileOutputStream = new FileOutputStream(file);
+                PrintWriter printWriter = new PrintWriter(fileOutputStream);
+                printWriter.write(json);
+                printWriter.flush();
+                printWriter.close();
+                fileOutputStream.close();
             }
 
         }catch(Exception e){
