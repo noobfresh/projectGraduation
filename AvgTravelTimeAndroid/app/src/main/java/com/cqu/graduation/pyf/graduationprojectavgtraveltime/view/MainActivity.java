8157c689c2a7af97276c1d4916712a7bb9194e02
noobfresh <954078940@qq.com>
2018-05-21 16:03:23 +0800

    display map

--- AvgTravelTimeAndroid/app/src/main/java/com/cqu/graduation/pyf/graduationprojectavgtraveltime/view/MainActivity.java
+++ AvgTravelTimeAndroid/app/src/main/java/com/cqu/graduation/pyf/graduationprojectavgtraveltime/view/MainActivity.java
@@ -6,17 +6,19 @@ import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 
-import com.amap.api.navi.AmapNaviPage;
-import com.amap.api.navi.AmapNaviParams;
-import com.amap.api.navi.INaviInfoCallback;
-import com.amap.api.navi.model.AMapNaviLocation;
+import com.amap.api.maps.AMap;
+import com.amap.api.maps.CameraUpdate;
+import com.amap.api.maps.CameraUpdateFactory;
+import com.amap.api.maps.MapView;
+import com.amap.api.maps.model.MyLocationStyle;
 import com.cqu.graduation.pyf.graduationprojectavgtraveltime.R;
 import com.cqu.graduation.pyf.graduationprojectavgtraveltime.databinding.ActivityMainBinding;
 import com.cqu.graduation.pyf.graduationprojectavgtraveltime.bean.UserBean;
 
-public class MainActivity extends AppCompatActivity implements INaviInfoCallback{
+public class MainActivity extends AppCompatActivity{
 
     private static final String TAG = "MainActivity";
+    private MapView mapView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -26,79 +28,53 @@ public class MainActivity extends AppCompatActivity implements INaviInfoCallback
                 R.layout.activity_main);
         UserBean userBean = new UserBean("PYF", 22);
         binding.setUser(userBean);
-        AmapNaviPage.getInstance().showRouteActivity(this, new AmapNaviParams(null),
-                this);
+
+        mapView = findViewById(R.id.map);
+        mapView.onCreate(savedInstanceState);
+        AMap aMap = mapView.getMap();
+        aMap.moveCamera(CameraUpdateFactory.zoomTo(15f));
+        //显示定位
+        MyLocationStyle myLocationStyle;
+        //初始化定位蓝点样式类
+        myLocationStyle = new MyLocationStyle();
+        
+        //连续定位、且将视角移动到地图中心点，定位点依照设备方向旋转，并且会跟随设备移动。
+        // （1秒1次定位）如果不设置myLocationType，默认也会执行此种模式。
+        myLocationStyle.myLocationType(MyLocationStyle.LOCATION_TYPE_LOCATION_ROTATE);
+        myLocationStyle.showMyLocation(true);
+        //设置连续定位模式下的定位间隔，
+        // 只在连续定位模式下生效，单次定位模式下不会生效。单位为毫秒。
+        myLocationStyle.interval(10000);
+        aMap.setMyLocationStyle(myLocationStyle);//设置定位蓝点的Style
+        aMap.getUiSettings().setMyLocationButtonEnabled(true);//设置默认定位按钮是否显示，非必需设置。
+        aMap.setMyLocationEnabled(true);// 设置为true表示启动显示定位蓝点，false表示隐藏定位蓝点并不进行定位，默认是false。
     }
 
     @Override
-    public void onInitNaviFailure() {
-        Log.d(TAG, "onInitNaviFailure: ");
+    protected void onDestroy() {
+        super.onDestroy();
+        mapView.onDestroy();
     }
 
     @Override
-    public void onGetNavigationText(String s) {
-        Log.d(TAG, "onGetNavigationText: ");
+    protected void onResume() {
+        super.onResume();
+        mapView.onResume();
+        Log.d(TAG, "onResume: ");
     }
 
     @Override
-    public void onLocationChange(AMapNaviLocation aMapNaviLocation) {
-        Log.d(TAG, "onLocationChange: ");
+    protected void onPause() {
+        super.onPause();
+        mapView.onPause();
+        Log.d(TAG, "onPause: ");
     }
 
     @Override
-    public void onArriveDestination(boolean b) {
-        Log.d(TAG, "onArriveDestination: ");
+    protected void onSaveInstanceState(Bundle outState) {
+        super.onSaveInstanceState(outState);
+        mapView.onSaveInstanceState(outState);
+        Log.d(TAG, "onSaveInstanceState: ");
     }
 
-    @Override
-    public void onStartNavi(int i) {
-        Log.d(TAG, "onStartNavi: ");
-    }
-
-    @Override
-    public void onCalculateRouteSuccess(int[] ints) {
-        Log.d(TAG, "onCalculateRouteSuccess: ");
-    }
-
-    @Override
-    public void onCalculateRouteFailure(int i) {
-        Log.d(TAG, "onCalculateRouteFailure: ");
-    }
-
-    @Override
-    public void onStopSpeaking() {
-        Log.d(TAG, "onStopSpeaking: ");
-    }
-
-    @Override
-    public void onReCalculateRoute(int i) {
-        Log.d(TAG, "onReCalculateRoute: ");
-    }
-
-    @Override
-    public void onExitPage(int i) {
-        Log.d(TAG, "onExitPage: ");
-    }
-
-    @Override
-    public void onStrategyChanged(int i) {
-        Log.d(TAG, "onStrategyChanged: ");
-    }
-
-    @Override
-    public View getCustomNaviBottomView() {
-        Log.d(TAG, "getCustomNaviBottomView: ");
-        return null;
-    }
-
-    @Override
-    public View getCustomNaviView() {
-        Log.d(TAG, "getCustomNaviView: ");
-        return null;
-    }
-
-    @Override
-    public void onArrivedWayPoint(int i) {
-        Log.d(TAG, "onArrivedWayPoint: ");
-    }
 }
