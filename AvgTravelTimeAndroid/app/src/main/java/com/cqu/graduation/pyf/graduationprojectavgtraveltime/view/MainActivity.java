6e50bff5a855fb6ba208f5ebcfa337fefc1c714f
noobfresh <954078940@qq.com>
2018-05-22 02:04:08 +0800

    map add

--- AvgTravelTimeAndroid/app/src/main/java/com/cqu/graduation/pyf/graduationprojectavgtraveltime/view/MainActivity.java
+++ AvgTravelTimeAndroid/app/src/main/java/com/cqu/graduation/pyf/graduationprojectavgtraveltime/view/MainActivity.java
@@ -10,10 +10,18 @@ import com.amap.api.maps.AMap;
 import com.amap.api.maps.CameraUpdate;
 import com.amap.api.maps.CameraUpdateFactory;
 import com.amap.api.maps.MapView;
+import com.amap.api.maps.model.LatLng;
+import com.amap.api.maps.model.Marker;
+import com.amap.api.maps.model.MarkerOptions;
 import com.amap.api.maps.model.MyLocationStyle;
+import com.amap.api.services.core.PoiItem;
+import com.amap.api.services.poisearch.PoiResult;
+import com.amap.api.services.poisearch.PoiSearch;
 import com.cqu.graduation.pyf.graduationprojectavgtraveltime.R;
 import com.cqu.graduation.pyf.graduationprojectavgtraveltime.databinding.ActivityMainBinding;
 import com.cqu.graduation.pyf.graduationprojectavgtraveltime.bean.UserBean;
+
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity{
 
@@ -29,9 +37,10 @@ public class MainActivity extends AppCompatActivity{
         UserBean userBean = new UserBean("PYF", 22);
         binding.setUser(userBean);
 
+        //引入高德
         mapView = findViewById(R.id.map);
         mapView.onCreate(savedInstanceState);
-        AMap aMap = mapView.getMap();
+        final AMap aMap = mapView.getMap();
         aMap.moveCamera(CameraUpdateFactory.zoomTo(15f));
         //显示定位
         MyLocationStyle myLocationStyle;
@@ -47,7 +56,37 @@ public class MainActivity extends AppCompatActivity{
         myLocationStyle.interval(10000);
         aMap.setMyLocationStyle(myLocationStyle);//设置定位蓝点的Style
         aMap.getUiSettings().setMyLocationButtonEnabled(true);//设置默认定位按钮是否显示，非必需设置。
-        aMap.setMyLocationEnabled(true);// 设置为true表示启动显示定位蓝点，false表示隐藏定位蓝点并不进行定位，默认是false。
+        // 设置为true表示启动显示定位蓝点，false表示隐藏定位蓝点并不进行定位，默认是false。
+        aMap.setMyLocationEnabled(true);
+
+        //
+        PoiSearch.Query query = new PoiSearch.Query("大学城", "", "023");
+        query.setPageSize(10);
+        query.setPageNum(1);
+
+        PoiSearch search = new PoiSearch(this, query);
+        search.setOnPoiSearchListener(new PoiSearch.OnPoiSearchListener() {
+            @Override
+            public void onPoiSearched(PoiResult poiResult, int i) {
+                Log.d(TAG, "onPoiSearched: " + i);
+                List<PoiItem> list = poiResult.getPois();
+
+                for(PoiItem item : list){
+
+                    aMap.addMarker(new MarkerOptions()
+                            .position(new LatLng(item.getLatLonPoint().getLatitude(),
+                                    item.getLatLonPoint().getLongitude()))
+                            .title(item.getAdName()).snippet("DefaultMarker"));
+                }
+            }
+
+            @Override
+            public void onPoiItemSearched(PoiItem poiItem, int i) {
+                Log.d(TAG, "onPoiItemSearched: ");
+            }
+        });
+
+        search.searchPOIAsyn();
     }
 
     @Override
